b46353ba577293409f45e1a3faa063eaa7e1e007
chloe posthuma-coelho <96831510+chloeposthumacoelho@users.noreply.github.com>
2022-10-17 10:51:11 -0400

    Add files via upload

---
+++ PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  insta
+//
+//  Created Created by Chloe Posthuma-Coelho on 10/17/2022
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
